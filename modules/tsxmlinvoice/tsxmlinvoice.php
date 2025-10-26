74ca48209968638ac37efb4b41e7ed106c95fc0e
limones1319 <gabone@gmail.com>
2025-10-26 17:39:49 +0200

    Register module autoloader for controller

--- modules/tsxmlinvoice/tsxmlinvoice.php
+++ modules/tsxmlinvoice/tsxmlinvoice.php
@@ -9,6 +9,25 @@ if (!defined('_PS_VERSION_')) {
     exit;
 }
 
+if (!defined('TSXMLINVOICE_AUTOLOAD_REGISTERED')) {
+    define('TSXMLINVOICE_AUTOLOAD_REGISTERED', true);
+
+    spl_autoload_register(function ($class) {
+        $prefix = 'Modules\\Tsxmlinvoice\\';
+
+        if (0 !== strpos($class, $prefix)) {
+            return;
+        }
+
+        $relativeClass = substr($class, strlen($prefix));
+        $file = __DIR__ . '/src/' . str_replace('\\', '/', $relativeClass) . '.php';
+
+        if (is_file($file)) {
+            require_once $file;
+        }
+    });
+}
+
 class Tsxmlinvoice extends Module
 {
     /** @var array */
