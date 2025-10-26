db85fd19ca09bb3281491a3feaf81be003cf0fbc
limones1319 <gabone@gmail.com>
2025-10-26 17:25:15 +0200

    Handle missing id when generating XML invoice route

--- modules/tsxmlinvoice/tsxmlinvoice.php
+++ modules/tsxmlinvoice/tsxmlinvoice.php
@@ -201,7 +201,7 @@ class Tsxmlinvoice extends Module
             'modules_tsxmlinvoice_generate',
             [
                 'id_order' => (int) $params['id_order'],
-                '_token' => $token,
+                'token' => $token,
             ],
             \Symfony\Component\Routing\Generator\UrlGeneratorInterface::ABSOLUTE_URL
         );
