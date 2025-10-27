0f1f70d383b979259377c346da028e169bd52e29
limones1319 <gabone@gmail.com>
2025-10-27 07:12:14 +0200

    Load XML invoice controller in module bootstrap

--- modules/tsxmlinvoice/config/bootstrap.php
+++ modules/tsxmlinvoice/config/bootstrap.php
@@ -1,9 +1,4 @@
 <?php
 
 require_once dirname(__DIR__) . '/autoload.php';
-
-if (class_exists('PrestaShopBundle\\Controller\\Admin\\FrameworkBundleAdminController')
-    && !class_exists('Modules\\Tsxmlinvoice\\Controller\\Admin\\XmlInvoiceController', false)
-) {
-    require_once dirname(__DIR__) . '/src/Controller/Admin/XmlInvoiceController.php';
-}
+require_once dirname(__DIR__) . '/src/Controller/Admin/XmlInvoiceController.php';
